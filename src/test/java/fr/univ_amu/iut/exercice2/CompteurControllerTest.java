596567bc23ab102a2122b2dafe929e1fd7658cb3
Younes SEHIL FARADJI <younes.SEHIL-FARADJI@etu.univ-amu.fr>
2026-05-21 14:07:46 +0000

    feat: exercice 2 - compteur fxml et controlleur terminee

--- src/test/java/fr/univ_amu/iut/exercice2/CompteurControllerTest.java
+++ src/test/java/fr/univ_amu/iut/exercice2/CompteurControllerTest.java
@@ -5,7 +5,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.stage.Stage;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
@@ -34,7 +33,6 @@ class CompteurControllerTest {
 
   // --- Étape 1 : le label affiche 0 au démarrage ---
 
-  @Disabled("Retire cette annotation pour activer le test")
   @Test
   @Order(1)
   void le_compteur_affiche_zero_au_demarrage(FxRobot robot) {
@@ -49,8 +47,6 @@ class CompteurControllerTest {
 
   // --- Étape 2 : le bouton + incrémente ---
 
-  @Disabled("Retire cette annotation pour activer le test")
-  @Test
   @Order(2)
   void le_bouton_plus_est_present_et_libelle_avec_plus(FxRobot robot) {
     Button boutonPlus = robot.lookup("#boutonIncrementer").queryAs(Button.class);
@@ -60,7 +56,6 @@ class CompteurControllerTest {
     assertThat(boutonPlus.getText()).as("le bouton incrémenter doit afficher \"+\"").contains("+");
   }
 
-  @Disabled("Retire cette annotation pour activer le test")
   @Test
   @Order(3)
   void cliquer_sur_le_bouton_plus_incremente_le_compteur_de_un(FxRobot robot) {
@@ -72,7 +67,6 @@ class CompteurControllerTest {
         .isEqualTo("1");
   }
 
-  @Disabled("Retire cette annotation pour activer le test")
   @Test
   @Order(4)
   void trois_clics_sur_plus_portent_le_compteur_a_trois(FxRobot robot) {
@@ -88,7 +82,6 @@ class CompteurControllerTest {
 
   // --- Étape 3 : le bouton − décrémente ---
 
-  @Disabled("Retire cette annotation pour activer le test")
   @Test
   @Order(5)
   void cliquer_sur_le_bouton_moins_decremente_le_compteur_de_un(FxRobot robot) {
@@ -100,7 +93,6 @@ class CompteurControllerTest {
         .isEqualTo("-1");
   }
 
-  @Disabled("Retire cette annotation pour activer le test")
   @Test
   @Order(6)
   void deux_increments_puis_deux_decrements_ramenent_le_compteur_a_zero(FxRobot robot) {
@@ -116,7 +108,6 @@ class CompteurControllerTest {
 
   // --- Étape 4 : le bouton Réinitialiser ---
 
-  @Disabled("Retire cette annotation pour activer le test")
   @Test
   @Order(7)
   void cliquer_sur_reinitialiser_remet_le_compteur_a_zero(FxRobot robot) {
@@ -128,7 +119,7 @@ class CompteurControllerTest {
     robot.interact(boutonReinit::fire);
     Label labelCompteur = robot.lookup("#labelCompteur").queryAs(Label.class);
     assertThat(labelCompteur.getText())
-        .as("après clic sur Réinitialiser, le label doit afficher \"0\"")
+        .as("après clic sur Réinitialiser, lcompteur.set(0);el doit afficher \"0\"")
         .isEqualTo("0");
   }
 }
